9981c2f120efcbc023399fbacc123b9913bfdc0d
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2024-05-28 03:37:54 +0530

    Sanity added
    Sanity Installed.

---
+++ print-shop-sanity/sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Print Shop  Sanity',
+
+  projectId: '5p5r6jrg',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
